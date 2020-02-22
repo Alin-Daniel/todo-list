90f57414bc8bab096ec14cca461107f4ddfe35e7
Alin-Daniel Alin-Daniel <daniel.alin100@gmail.com>
2020-02-22 17:33:16 +0200

    'Update

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const store = createStore(rootReducer, composeEnhancers(
 
 const app = (
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename='/todo-list'>
       <App />
     </BrowserRouter>
   </Provider>
