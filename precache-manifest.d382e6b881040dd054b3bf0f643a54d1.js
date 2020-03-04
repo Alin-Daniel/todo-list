b1f8f52f620d2029edc27472d3627830c4b0218c
Alin-Daniel Alin-Daniel <daniel.alin100@gmail.com>
2020-03-04 10:06:24 +0200

    Updates

---
+++ precache-manifest.d382e6b881040dd054b3bf0f643a54d1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a9c493f9dbb95ffe46dd0f77d4660bd",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "b809677c696052a3a2bc",
+    "url": "/todo-list/static/css/3.277ad1db.chunk.css"
+  },
+  {
+    "revision": "a85430018ffaaf7c2191",
+    "url": "/todo-list/static/css/4.4a119fd4.chunk.css"
+  },
+  {
+    "revision": "6e5a0f81017cdd8501d1",
+    "url": "/todo-list/static/css/main.cd258e62.chunk.css"
+  },
+  {
+    "revision": "e01268a7add664550184",
+    "url": "/todo-list/static/js/2.b4215e4f.chunk.js"
+  },
+  {
+    "revision": "b809677c696052a3a2bc",
+    "url": "/todo-list/static/js/3.8066e671.chunk.js"
+  },
+  {
+    "revision": "a85430018ffaaf7c2191",
+    "url": "/todo-list/static/js/4.561d970e.chunk.js"
+  },
+  {
+    "revision": "6e5a0f81017cdd8501d1",
+    "url": "/todo-list/static/js/main.9da3a8f3.chunk.js"
+  },
+  {
+    "revision": "1c9b0132e79ac20966d5",
+    "url": "/todo-list/static/js/runtime~main.e723536e.js"
+  },
+  {
+    "revision": "80687d23d8eef70e255e1996a06d9f5c",
+    "url": "/todo-list/static/media/react-logo.80687d23.png"
+  }
+]);
